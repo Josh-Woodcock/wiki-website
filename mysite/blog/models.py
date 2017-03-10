921dcd71bb21c5f82acdf4d2e7dc742eff1b9c01
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-03-10 15:13:36 +0000

    -added Event Page

---
+++ mysite/blog/models.py
@@ -0,0 +1,134 @@
+from django import forms
+from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.tags import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import StreamField, RichTextField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtailsearch import index
+
+from wagtail.wagtailsnippets.models import register_snippet
+
+
+
+# PAGE MODELS
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('BlogPage', related_name='tagged_items')
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('EventPage', related_name='tagged_items')
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super(BlogTagIndexPage, self).get_context(request)
+        context['blogpages'] = blogpages
+        return context
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        ImageChooserPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
+
+class BlogPage(Page):
+    author = models.CharField(max_length=255)
+    date = models.DateField("Post date")
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+        MultiFieldPanel([
+            FieldPanel('author'),
+            FieldPanel('date'),
+            FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Article information"),
+    ]
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.SearchField('author'),
+    ]
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
+class EventPage(Page):
+    organiser =  models.CharField(max_length=255)
+    date_time = models.DateTimeField('Event Date')
+    location = models.CharField(max_length=255)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+        FieldPanel('organiser'),
+        FieldPanel('date_time'),
+        FieldPanel('location'),
+        FieldPanel('tags'),
+    ], heading="Event Information"),
+        StreamFieldPanel('body'),
+    ]
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.SearchField('date_time'),
+        index.SearchField('location'),
+        index.SearchField('tags'),
+
+    ]
+
