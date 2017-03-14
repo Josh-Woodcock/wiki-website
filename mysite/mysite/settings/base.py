960031a295c48881f68611aca4865c5b1347f802
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-03-14 16:32:48 +0000

    - ourresearch model and template complete. Template needs refining or icons need changing.

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'blog',
     'casestudies',
     'FAQ',
+    'ourresearch',
 
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
--- mysite/ourresearch/models.py
+++ mysite/ourresearch/models.py
@@ -1,3 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
 from django.db import models
 
+from wagtail.wagtailcore.models import Page
+
 # Create your models here.
+
+
+class OurResearchIndex(Page):
+    pass
