98bd9e2ad7c182a1b3dc83b3b8048a203279b245
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-03-21 10:06:22 +0000

    created about app, scratches of template and model

--- mysite/casestudies/models.py
+++ mysite/casestudies/models.py
@@ -24,6 +24,7 @@ from wagtail.contrib.table_block.blocks import TableBlock
 
 # Create your models here.
 
+
 class CaseStudyIndex(Page):
     intro = RichTextField(blank=True)
 
@@ -32,20 +33,46 @@ class CaseStudyIndex(Page):
     ]
 
 
-
 class CaseStudy(Page):
-    uni = models.CharField(max_length=255)
+
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
-        ('image', ImageChooserBlock()),
         ('document', DocumentChooserBlock()),
     ])
 
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
     content_panels = Page.content_panels + [
+        InlinePanel('gallery_images', label="Case Study Image"),
         StreamFieldPanel('body'),
     ]
 
     search_fields = Page.search_fields + [
         index.SearchField('body'),
+    ]
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+
+class CaseStudyGalleryImage(Orderable):
+    page = ParentalKey(CaseStudy, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
